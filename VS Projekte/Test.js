c35f43145af00266d8447ebef6f393f7e57fdc37
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-01-20 17:02:36 +0100

    Update Test.js

--- VS Projekte/Test.js
+++ VS Projekte/Test.js
@@ -1,8 +1,41 @@
+const keyword = 'keyword';
+const identifier = 'identifier';
+const keywords = [
+    'if',
+    'elif',
+    'else',
+    'while',
+    'do',
+    'for',
+    'each',
+    'class',
+    'function',
+    'struct',
+    'public',
+    'private',
+    'protected',
+    'const',
+    'static',
+    'void',
+    'override',
+    'operation',
+    'return',
+    'continue',
+    'break',
+    'sizeof',
+    'typeof',
+    'lenghtof',
+    'using',
+    'namespace'
+];
+
 const Token = {
-    pos = {
+    pos : {
         row: 0,
         col: 0
-    }
+    },
+    value : '',
+    type : ''
 };
 
 
