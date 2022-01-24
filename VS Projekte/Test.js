77bbc061b4997b4de0f499e74dbbee9ed5f4638a
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-01-24 14:48:55 +0100

    Update Test.js

--- VS Projekte/Test.js
+++ VS Projekte/Test.js
@@ -1,181 +1,7 @@
-// Token types
-const operator = 'operator';       // pale green
-const keyword = 'keyword';         // dark pink
-const vartype = 'type';            // dark blue
-const identifier = 'identifier';   // light blue
-const textelement = 'textelement'; // white
-const text = 'text';               // dark orange
-const number = 'number';           // pale yellow
-const comment = 'comment';         // dark green
-const newline = 'newline';
-const whitespace = 'whitespace';
-
-// Predefined token values
-// Keywords
-const keywords = [
-    'if',
-    'elif',
-    'else',
-    'while',
-    'do',
-    'for',
-    'each',
-    'class',
-    'function',
-    'struct',
-    'public',
-    'private',
-    'protected',
-    'const',
-    'static',
-    'void',
-    'override',
-    'operation',
-    'return',
-    'continue',
-    'break',
-    'sizeof',
-    'typeof',
-    'lenghtof',
-    'using',
-    'namespace',
-    'null',
-    'true',
-    'false',
-    'var',
-    'byt',
-    'chr',
-    'str',
-    'int',
-    'flt',
-    'dbl',
-    'bol',
-    'typ'
-];
-
-// Variable types
-const types = [
-    'var',
-    'byt',
-    'chr',
-    'str',
-    'int',
-    'flt',
-    'dbl',
-    'bol',
-    'typ',
-    'void'
-];
-
-// Operations
-const PLUS = '+';
-const MINUS = '-';
-const DIVIDE = '/';
-const MULTIPLY = '*';
-const MODULUS = '%';
-const POWER = '^';
-const ISEQUALTO = '?';
-const EQUALS = '=';
-const NOT = '!';
-const LESS = '<';
-const GREATER = '>';
-const QUOTE = '"';
-const SQUOTE = '\'';
-const DOT = '.';
-const OR = '|';
-const AND = '&';
-const BYTESTART = '$';
-const COLON = ':';
-
-const ops = [
-    PLUS,
-    MINUS,
-    DIVIDE,
-    MULTIPLY,
-    MODULUS,
-    POWER,
-    ISEQUALTO,
-    EQUALS,
-    NOT,
-    LESS,
-    GREATER,
-    QUOTE,
-    SQUOTE,
-    DOT,
-    OR,
-    AND,
-    BYTESTART,
-    COLON
-];
-
-// Code structure
-const leftCB = '{';
-const rightCB = '}';
-const leftSB = '[';
-const rightSB = ']';
-const leftB = '(';
-const rightB = ')';
-const endcolumn = ';';
-const comma = ',';
-const hashtag = '#';
-
-const codeStructure = [
-    leftCB,
-    rightCB,
-    leftSB,
-    rightSB,
-    leftB,
-    rightB,
-    endcolumn,
-    comma,
-    hashtag
-];
-
-const NL = '\n';
-const TAB = '\t';
-const EOF = 'endoffile';
-
-const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const digits = '0123456789';
-const letters_digits = letters + digits;
+var c = require("./Constant.js");
+console.log(c);
 
 // Editor data
-function genLine(ln, max_rowIdx) {
-    lineData.push({
-        ln : ln,
-        max_rowIdx : max_rowIdx
-    });
-}
-
-// If the shift or alt key is pressed specific characters will output special characters.
-const shiftChars = {
-    '1' : '!',
-    '2' : '"',
-    '3' : '§',
-    '4' : '$',
-    '5' : '%',
-    '6' : '&',
-    '7' : '/',
-    '8' : '(',
-    '9' : ')',
-    '0' : '=',
-    'ß' : '?',
-    '#' : '\'',
-    '+' : '*',
-    ',' : ';',
-    '.' : ':',
-    '-' : '_',
-    '<' : '>'
-}
-const altChars = {
-    '5' : '[',
-    '6' : ']',
-    '7' : '|',
-    '8' : '{',
-    '9' : '}',
-    'l' : '@'
-}
-
 const currentPos = {
     currLn : 0,
     currRow : 0,
@@ -193,46 +19,52 @@ function genTok(row, value, type) {
         type: type
     };
 }
+function genLine(ln, max_rowIdx) {
+    lineData.push({
+        ln : ln,
+        max_rowIdx : max_rowIdx
+    });
+}
 
 function highlight_code(tokens) {
     var output = '<p>';
     tokens.forEach(token => {
         //console.log(token);
-        if ([newline, whitespace].includes(token.type))
+        if ([c.newline, c.whitespace].includes(token.type))
             output += '<span>';
         else
             output += '<span style="color: ';
 
         switch (token.type) {
-            case operator:
+            case c.operator:
                 output += '#bffaa0">';
                 break;
-            case keyword:
+            case c.keyword:
                 output += '#faa0ec">';
                 break;
-            case vartype:
+            case c.vartype:
                 output += '#91abff">';
                 break;
-            case identifier:
+            case c.identifier:
                 output += '#72ceed">';
                 break;
-            case textelement:
+            case c.textelement:
                 output += '#dbdbdb">';
                 break;
-            case text:
+            case c.text:
                 output += '#c97026">';
                 break;
-            case number:
+            case c.number:
                 output += '#ffde85">';
                 break;
-            case comment:
+            case c.comment:
                 token.value = token.value.replace(/\n/i, '<br>');
                 output += '#507a43">';
                 break;
-            case whitespace:
+            case c.whitespace:
                 output += '&nbsp;';
                 break;
-            case newline:
+            case c.newline:
                 output += '<br>';
                 break;
         }
@@ -269,7 +101,7 @@ function lexing(code) {
             char = code[idx];
         }
         else
-            char = EOF;
+            char = c.EOF;
         //console.log(char + " : Advace from '" + advFrom + "'");
         return char;
     }
@@ -277,57 +109,57 @@ function lexing(code) {
         var str = '';
 
         //console.log(`Check if ${char} is inside ${letters} : ${(letters + '_').includes(char)}`);
-        while ((letters_digits + '_').includes(char)) {
+        while ((c.letters_digits + '_').includes(char)) {
             str += char;
             advance('genIdentifier');
         }
 
-        type = keywords.includes(str) ? keyword : identifier;
-        type = types.includes(str) ? vartype : type;
-        type = Number.isInteger(Number.parseInt(str)) ? number : type;
+        type = c.keywords.includes(str) ? c.keyword : c.identifier;
+        type = c.types.includes(str) ? c.vartype : type;
+        type = Number.isInteger(Number.parseInt(str)) ? c.number : type;
         value = str;
     }
     const genCommentOrDivide = () => {
         var str = char;
         advance();
-        if (char !== DIVIDE && char !== MULTIPLY) {
+        if (char !== c.ops.DIVIDE && char !== c.ops.MULTIPLY) {
             value = str;
             return;
         }
-        else if (char === DIVIDE) {
+        else if (char === c.ops.DIVIDE) {
             type = comment;
             
             str += char;
             advance();
             str += char;
             
-            while (char !== NL) {
-                if (char === EOF)
+            while (char !== c.NL) {
+                if (char === c.EOF)
                     break;
                 advance();
                 str += char;
             }
         }
-        else if (char === MULTIPLY) {
+        else if (char === c.ops.MULTIPLY) {
             type = comment;
             
             str += char;
             advance();
             str += char;
 
-            while (char != EOF) {
-                if (char === MULTIPLY) {
+            while (char != c.EOF) {
+                if (char === c.ops.MULTIPLY) {
                     advance();
-                    if (char !== EOF)
+                    if (char !== c.EOF)
                         str += char;
                     
-                        if (char === DIVIDE) {
+                        if (char === c.ops.DIVIDE) {
                         advance();
                         break;
                     }
                 }
                 advance();
-                if (char !== EOF)
+                if (char !== c.EOF)
                     str += char;
             }
         }
@@ -338,29 +170,29 @@ function lexing(code) {
     // Loop through all the characters
     while (true) {
         //console.warn(char);
-        if (char === EOF) break;
+        if (char === c.EOF) break;
 
         switch (char) {
-            case PLUS:
-            case MINUS:
-            case MULTIPLY:
-            case MODULUS:
-            case POWER:
-            case EQUALS:
-            case ISEQUALTO:
-            case NOT:
-            case LESS:
-            case GREATER:
-            case DOT:
-            case AND:
-            case OR:
-            case BYTESTART:
-            case COLON:
-                type = operator;
+            case c.ops.PLUS:
+            case c.ops.MINUS:
+            case c.ops.MULTIPLY:
+            case c.ops.MODULUS:
+            case c.ops.POWER:
+            case c.ops.EQUALS:
+            case c.ops.ISEQUALTO:
+            case c.ops.NOT:
+            case c.ops.LESS:
+            case c.ops.GREATER:
+            case c.ops.DOT:
+            case c.ops.AND:
+            case c.ops.OR:
+            case c.ops.BYTESTART:
+            case c.ops.COLON:
+                type = c.operator;
                 value = char;
                 advance('std while (l.301)');
                 break;
-            case NL:
+            case c.NL:
                 // Save current line data
                 genLine(ln, row);
 
@@ -369,35 +201,35 @@ function lexing(code) {
                 row = 0;
 
                 // Safe the newline token
-                value = NL;
-                type = newline;
+                value = c.NL;
+                type = c.newline;
                 advance('std while (l.307)');
                 break;
-            case DIVIDE:
+            case c.ops.DIVIDE:
                 genCommentOrDivide();
                 break;
-            case leftCB:
-            case rightCB:
-            case leftSB:
-            case rightSB:
-            case leftB:
-            case rightB:
-            case endcolumn:
-            case hashtag:
-            case comma:
-                type = textelement;
+            case c.codeStructure.leftCB:
+            case c.codeStructure.rightCB:
+            case c.codeStructure.leftSB:
+            case c.codeStructure.rightSB:
+            case c.codeStructure.leftB:
+            case c.codeStructure.rightB:
+            case c.codeStructure.endcolumn:
+            case c.codeStructure.hashtag:
+            case c.codeStructure.comma:
+                type = c.textelement;
                 value = char;
                 advance('std while (l.324)');
                 break;
-            case QUOTE:
-            case SQUOTE:
-                type = text;
+            case c.ops.QUOTE:
+            case c.ops.SQUOTE:
+                type = c.text;
                 value = char;
                 advance('std while (l.330)');
                 break;
             case undefined:
             case ' ':
-                type = whitespace;
+                type = c.whitespace;
                 value = '';
                 advance('std while');
                 break;
@@ -412,39 +244,45 @@ function lexing(code) {
     highlight_code(tokens);
 }
 
-function getCode() {
-    return document.getElementsByName("editor")[0].value;
-}
-
-
 function init() {
+    console.log("Called init function.");
     document.addEventListener('keydown', function (e) {
         console.log(e.code);
+
         switch (e.code) {
             case 'AltLeft':
             case 'ShiftLeft':
                 currentPos[e.code] = true;
                 break;
+            case 'Enter':
+                current_code += '\n';
+                break;
+            case 'Backspace':
+                if (current_code.length > 0)
+                    current_code = current_code.slice(0, -1);
+                break;
             default:
-                var currChar = String.fromCharCode(e.code);
+                var char = c.getCharFromKeycode(e.code);
+                
+                if (!char) break;
+
                 if (currentPos.AltLeft) {
-                    if (altChars.hasOwnProperty(currChar))
-                        current_code += altChars[currChar];
+                    if (c.altChars.hasOwnProperty(char))
+                        current_code += c.altChars[char];
                 }
                 else if (currentPos.ShiftLeft) {
-                    if (shiftChars.hasOwnProperty(currChar))
-                        current_code += shiftChars[currChar];
+                    if (c.shiftChars.hasOwnProperty(char))
+                        current_code += c.shiftChars[char];
                     else
-                        current_code += currChar.toUpperCase();
+                        current_code += char.toUpperCase();
                 }
                 else {
-                    current_code += currChar;
+                    current_code += char;
                 }
-
-                console.log(current_code);
-                //lexing(current_code);
                 break;
         }
+
+        lexing(current_code);
     });
     document.addEventListener('keyup', function (e) {
         switch (e.code) {
@@ -455,3 +293,5 @@ function init() {
         }
     });
 }
+
+init();
