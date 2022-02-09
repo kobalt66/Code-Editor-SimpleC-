77727b92c8b06de0cff03f1ab07dc504dcb49b31
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-02-09 22:01:51 +0100

    Update pythonServer.py

--- python_stuff/pythonServer.py
+++ python_stuff/pythonServer.py
@@ -2,38 +2,71 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from json import loads
 from sys import argv
 from SimpleC import runScript
+from subprocess import call, run, PIPE
 
-BIND_HOST = 'localhost'
+BIND_HOST = '192.168.178.58'
 PORT = 8008
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_length = int(self.headers.get('content-length', 0))
-        body = self.rfile.read(content_length)
+    def do_GET(self): 
+        try:  
+            # Setup 
+            self.send_response(200) 
+            self.send_header('Access-Control-Allow-Origin', '*') 
+            self.send_header("Content-Type", "application/json") 
+            self.end_headers() 
+            
+            # Process data 
+            call(['mcs', 'output.cs']) 
+            result = run(['mono', 'output.exe'], stdout=PIPE) 
+            print('Sending data: ', result.stdout.decode('utf-8')) 
+            print("Success!") 
+            
+            json = "{\"result\" : \"" + str(result.stdout.decode('utf-8')) + "\"}" 
+            self.wfile.write(json.encode('utf-8')) 
         
-        self.write_response(body)
-
-    def write_response(self, content):
+        except Exception as e: 
+            self.wfile.write("[GET] Something went wrong while processing the data!".encode('utf-8'))
+            print("Ups, something went wrong!")
+            print(e) 
+            
+    def do_POST(self): 
+        # Setup
+        self.send_response(200) 
+        self.send_header('Access-Control-Allow-Origin', '*') 
+        self.end_headers() 
+        
+        content_length = int(self.headers.get('content-length', 0)) 
+        body = self.rfile.read(content_length) 
+        
+        # Process data
         self.send_response(200)
-        self.end_headers()
+        self.end_headers() 
         
-        codeJSON = content.decode('utf-8')
-        code = loads(codeJSON)['code']
+        try: 
+            codeJSON = body.decode('utf-8') 
+            code = loads(codeJSON)['code'] 
+            result = runScript('js_test', code)
         
-        print(runScript('js_test', code))
+            print(result) 
+            print("Success!") 
         
-        # run the script and get the result
+            json = "{\"result\" : \"" + result + "\"}" 
+            self.wfile.write(json.encode('utf-8')) 
         
-        print('Success!')
-
-
-if len(argv) > 1:
-    arg = argv[1].split(':')
-    BIND_HOST = arg[0]
+        except Exception as e: 
+            self.wfile.write("[POST] Something went wrong while processing the data!".encode('utf-8'))
+            print("Ups, something went wrong!") 
+            print(e) 
+            
+            
+if len(argv) > 1: 
+    arg = argv[1].split(':') 
+    BIND_HOST = arg[0] 
     PORT = int(arg[1])
+    
+print(f'Listening on http://{BIND_HOST}:{PORT}\n') 
 
-print(f'Listening on http://{BIND_HOST}:{PORT}\n')
-
-httpd = HTTPServer((BIND_HOST, PORT), SimpleHTTPRequestHandler)
+httpd = HTTPServer((BIND_HOST, PORT), SimpleHTTPRequestHandler) 
 httpd.serve_forever()
