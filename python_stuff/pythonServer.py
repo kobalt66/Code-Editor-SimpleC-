4d05f8dcde416876ab7a507f8642cab0766f5568
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-02-13 12:35:46 +0100

    Update pythonServer.py

--- python_stuff/pythonServer.py
+++ python_stuff/pythonServer.py
@@ -3,75 +3,109 @@ from json import loads
 from sys import argv
 from SimpleC import runScript
 from subprocess import call, run, PIPE
+from os import path
 
 BIND_HOST = '192.168.178.58'
 PORT = 8008
+PROJECTS = '/home/pi/Desktop/SimpleC/Code-Editor-SimpleC-/python_stuff/Projects'
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self): 
-        try:  
-            # Setup 
-            self.send_response(200) 
-            self.send_header('Access-Control-Allow-Origin', '*') 
-            self.send_header("Content-Type", "application/json") 
-            self.end_headers() 
+    def do_GET(self):
+        try:
+            # Setup
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.send_header("Content-Type", "application/json")
+            self.send_header("Mode", "no-cors")
+            self.end_headers()
             
-            # Process data 
+            # Process data
             result = run(['mcs', 'output.cs'], stdout=PIPE, stderr=PIPE)
             if not result.stderr == b'':
-                json = "{\"result\" : \"" + str(result.stdout.decode('utf-8')) + "\", \"result\" : \"" + str(result.stderr.decode('utf-8')) + "\"}" 
+                json = result.stderr.decode('utf-8')
                 self.wfile.write(json.encode('utf-8'))
                 print("Success!")
                 return
-            
-            result = run(['mono', 'output.exe'], stdout=PIPE, stderr=PIPE) 
-            json = "{\"result\" : \"" + str(result.stdout.decode('utf-8')) + "\", \"result\" : \"" + str(result.stderr.decode('utf-8')) + "\"}" 
+                
+            result = run(['mono', 'output.exe'], stdout=PIPE, stderr=PIPE)
+            json = result.stderr.decode('utf-8') + "<br><br>" + result.stdout.decode('utf-8')
             self.wfile.write(json.encode('utf-8'))
             print("Success!")
-        
-        except Exception as e: 
+            
+        except Exception as e:
             self.wfile.write("[GET] Something went wrong while processing the data!".encode('utf-8'))
             print("Ups, something went wrong!")
-            print(e) 
-            
-    def do_POST(self): 
+            print(e)
+        
+    def do_POST(self):
         # Setup
-        self.send_response(200) 
-        self.send_header('Access-Control-Allow-Origin', '*') 
-        self.end_headers() 
-        
-        content_length = int(self.headers.get('content-length', 0)) 
-        body = self.rfile.read(content_length) 
-        
-        # Process data
         self.send_response(200)
-        self.end_headers() 
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
         
-        try: 
-            codeJSON = body.decode('utf-8') 
-            code = loads(codeJSON)['code'] 
+        content_length = int(self.headers.get('content-length', 0))
+        body = self.rfile.read(content_length)
+        
+        codeJSON = body.decode('utf-8')
+        code = loads(codeJSON)['code']
+        
+        
+        
+            
+            
+class PostRequest:
+    def do_SAVEPROJECT(content):
+        # Process data
+        try:
+            # Extract data from JSON
+            JSON = content.decode('utf-8')
+            obj = loads(codeJSON)
+            
+            projectTag = obj['tag']
+            code = obj['code']
+            
+            # Saving data
+            projectPath = PROJECTS + "/" + projectTag
+            
+            file = open(projectPath, 'x')
+            file.write(code)
+            file.close()
+            
+            return f"Successfully saved {projectTag}!".encode('utf-8')
+            print(f"Successfully saved {projectTag}!")
+            
+        except Exception as e:
+            return f"[SAVEPROJECT] Something went wrong while saving {projectTag}!".encode('utf-8')
+            print(f"[SAVEPROJECT] Something went wrong while saving {projectTag}!")
+            print(e)
+            
+    def POST(body):
+        # Process data
+        try:
             result = runScript('js_test', code)
-        
+            
             if result:
-                json = "{\"result\" : \"" + result + "\"}" 
-                self.wfile.write(json.encode('utf-8')) 
+                json = result
+                return json.encode('utf-8')
             else:
-                self.wfile.write("Successfully compiled the code!".encode('utf-8'))
+                return "Successfully compiled the code!".encode('utf-8')
             print("Success!")
+            
+        except Exception as e:
+            return "[POST] Something went wrong while processing the data!".encode('utf-8')
+            print("[POST] Something went wrong while processing the data!")
+            print(e)
         
-        except Exception as e: 
-            self.wfile.write("[POST] Something went wrong while processing the data!".encode('utf-8'))
-            print("Ups, something went wrong!") 
-            print(e) 
-            
-            
-if len(argv) > 1: 
-    arg = argv[1].split(':') 
-    BIND_HOST = arg[0] 
-    PORT = int(arg[1])
-    
-print(f'Listening on http://{BIND_HOST}:{PORT}\n') 
+        
 
-httpd = HTTPServer((BIND_HOST, PORT), SimpleHTTPRequestHandler) 
+
+if len(argv) > 1:
+    arg = argv[1].split(':')
+    BIND_HOST = arg[0]
+    PORT = int(arg[1])
+
+print(f'Listening on http://{BIND_HOST}:{PORT}\n')
+
+httpd = HTTPServer((BIND_HOST, PORT), SimpleHTTPRequestHandler)
 httpd.serve_forever()
