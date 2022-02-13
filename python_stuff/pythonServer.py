bfe45cd1e8155354d52aaf114f5ad3000c31d96e
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-02-13 17:58:02 +0100

    Update pythonServer.py

--- python_stuff/pythonServer.py
+++ python_stuff/pythonServer.py
@@ -1,8 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from json import loads
 from sys import argv
-from subprocess import call, run, PIPE
 from PostRequests import SAVESCRIPT, COMPILE
+from GetRequests import RUN, LOADPROJECTS, GETCODE
 
 BIND_HOST = '192.168.178.58'
 PORT = 8008
@@ -18,18 +18,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_header("Mode", "no-cors")
             self.end_headers()
             
-            # Process data
-            result = run(['mcs', 'output.cs'], stdout=PIPE, stderr=PIPE)
-            if not result.stderr == b'':
-                json = result.stderr.decode('utf-8')
-                self.wfile.write(json.encode('utf-8'))
-                print("Success!")
-                return
-                
-            result = run(['mono', 'output.exe'], stdout=PIPE, stderr=PIPE)
-            json = result.stderr.decode('utf-8') + "<br><br>" + result.stdout.decode('utf-8')
-            self.wfile.write(json.encode('utf-8'))
-            print("Success!")
+            output = RUN()
+            print(output.decode('utf-8'))
+            self.wfile.write(output)
             
         except Exception as e:
             self.wfile.write("[GET] Something went wrong while processing the data!".encode('utf-8'))
@@ -37,28 +28,33 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             print(e)
         
     def do_POST(self):
-        # Setup
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        
-        content_length = int(self.headers.get('content-length', 0))
-        body = self.rfile.read(content_length)
-        
-        JSON = body.decode('utf-8')
-        _type = loads(JSON)['type']
-        
-        output = ''
-        if _type == 'SAVESCRIPT':
-            output = SAVESCRIPT(body)
-        elif _type == 'COMPILE':
-            output = COMPILE(body)
-        
-        print(output.decode('utf-8'))
-        self.wfile.write(output)
-        
-        
-
+        try:
+            # Setup
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            
+            content_length = int(self.headers.get('content-length', 0))
+            body = self.rfile.read(content_length)
+            
+            JSON = body.decode('utf-8')
+            _type = loads(JSON)['type']
+            
+            output = ''
+            if _type == 'SAVESCRIPT':
+                output = SAVESCRIPT(body)
+            elif _type == 'COMPILE':
+                output = COMPILE(body)
+            elif _type == 'LOADPROJECTS':
+                output = LOADPROJECTS()
+            elif _type == 'GETCODE':
+                output = GETCODE(body)
+                
+            print(output.decode('utf-8'))
+            self.wfile.write(output)
+            
+        except Exception as e:
+            self.wfile.write((f"[POST] Something went wrong!" + str(e)).encode('utf-8'))
 
 if len(argv) > 1:
     arg = argv[1].split(':')
