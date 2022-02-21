1962911171ad1ddf9b38f5fbbbc81637817118fd
LePhobix_ <40542349+kobalt66@users.noreply.github.com>
2022-02-21 21:08:44 +0100

    Create filesubmit.js

---
+++ VS Projekte/filesubmit.js
@@ -0,0 +1,30 @@
+import { letters_digits } from "./Constant.js"
+import { throwWarn, submitFile } from "./Test.js"
+
+window.searchToggle = searchToggle;
+
+function searchToggle(obj, evt) {
+    var container = $(obj).closest('.search-wrapper');
+    if (!container.hasClass('active')) {
+        container.addClass('active');
+        evt.preventDefault();
+    }
+    else if (container.hasClass('active') && $(obj).closest('.input-holder').length == 0) {
+        var path = container.find('.search-input').val();
+
+        if (path === '') {
+            container.removeClass('active');
+            container.find('.search-input').val('');
+            return;
+        }
+        else if (Array.from(letters_digits + '/.').every(l => { return !path.includes(l); }) || path.includes(' ')) 
+        {
+            throwWarn("Please enter a valid file path!");
+            return;
+        }
+
+        submitFile(path);
+        container.removeClass('active');
+        container.find('.search-input').val('');
+    }
+}
